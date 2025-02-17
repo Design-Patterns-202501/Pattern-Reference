78a30a174910cfdef16d0772dbfb8442e9053db0
Aulas ingenieria <aulasingenieria@12C302E06190847.ujaverianas.loc>
2025-02-17 17:41:59 -0500

    Start factory method

---
+++ factory_method/src/main/java/com/patrones/entities/Article.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Article {
+    private Long id;
+    private String name;
+    private Double value;
+
+    public Article(Long id, String name, Double value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public Article() {}
+
+    public Long getId() { return this.id; };
+    public void setId(Long id) { this.id = id; };
+
+    public String getName() { return this.name; };
+    public void setName(String name) { this.name = name; };
+
+    public Double getValue() { return this.value; };
+    public void setValue(Double value) { this.value = value; };
+};
